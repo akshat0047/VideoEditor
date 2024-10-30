2e7982f0e933cf9b15a813ea2f8767452eea4d77
Akshat Pande <akshat.pande@joingsg.com>
2024-10-30 14:01:28 +0530

    feat: add controllers for links

---
+++ video-editor-server/src/controllers/links.controller.ts
@@ -0,0 +1,32 @@
+import linkRepo from '../repositories/link.repository';
+import videoRepo from '../repositories/video.repository';
+import { Request, Response } from 'express'
+import moment from 'moment';
+import Link from '../models/link';
+
+export default class LinkController {
+
+    static async getLinkByVideo(req: Request, res: Response, next: Function) {
+        let link = await linkRepo.getLinkByVideoId(req.params.id);
+        return res.send({ link });
+    };
+
+    static async createLink(req: Request, res: Response, next: Function) {
+        const { videoId, expiryTime} = req.body;
+        const video = await videoRepo.getVideoById(videoId);
+
+        // Generate a temporary link for localhost
+        const temporaryLink = `http://localhost:${process.env.PORT}/uploads/${video.fileName}`;
+        const expiry = moment().add(expiryTime, 'minutes').toISOString();
+
+        const link = new Link(videoId, temporaryLink, expiry)
+        await linkRepo.createLink(link); 
+        
+        return res.status(201).json({ message: 'Link created successfully', link: temporaryLink });
+    };
+    
+    static async syncExpiredLinks(req: Request, res: Response, next: Function) {
+        await linkRepo.deleteExpiredLinks();
+        return res.status(200).json({ message: 'Expired links deleted successfully'});
+    };
+}
