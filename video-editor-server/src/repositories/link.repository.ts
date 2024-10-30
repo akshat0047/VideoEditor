7ad1a0df216f5292465fc3301c9e7ed20b2dbae4
Akshat Pande <akshat.pande@joingsg.com>
2024-10-30 14:00:36 +0530

    feat: add repo for link model

---
+++ video-editor-server/src/repositories/link.repository.ts
@@ -0,0 +1,32 @@
+import dao from "./dao";
+import Link from "../models/link";
+import { uuidv4 } from 'uuidv7';
+
+export default class {
+    static async createLink(link: Link): Promise<boolean> {
+        const stmt = "INSERT INTO links (id, videoId, temporaryLink, expiryTime) VALUES (?, ?, ?, ?)" 
+        try {
+            await dao.run(stmt, [uuidv4(), link.videoId, link.temporaryLink, link.expiryTime]);
+            return true;
+        } catch (err) {
+            console.error(err);
+            return false;
+        }
+    }
+
+    static async getLinkByVideoId(videoId: string): Promise<Link> {
+        const link = await dao.get("SELECT * FROM links WHERE videoId = ?", [videoId])
+        return <Link>link;
+    }
+
+    static async deleteExpiredLinks() {
+       const stmt = `DELETE FROM links WHERE expiryTime < ?`;
+       try {
+            await dao.run(stmt, [new Date().toISOString()]);
+            return true;
+        } catch (err) {
+            console.error(err);
+            return false;
+        }
+    }
+}
